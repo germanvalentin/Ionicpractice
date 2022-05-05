1635a263c8f03cad40dc065e4461b7bd6133a7ce
Germann <germanvalentin27@gmail.com>
2022-05-05 20:05:50 -0300

    Busqueda agregada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,31 +7,35 @@ import {Gasto} from '../interfaces/gastos.interface';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
-  titulobuscar ='';
   gastos: Gasto[]=[];
-  gastosfiltrado = this.gastos.filter(gasto => gasto.titulo.includes(this.titulobuscar));
+  gastosfiltrado: Gasto[]=[];
   model: Gasto = {id: '', titulo: '', importe: '' , descripcion: '', fecha: '' , categoria: '',};
 
     constructor(private anotadorservice: AnotadorService)  { 
  
   }
-  
+
     ngOnInit(): void {
-    
+
       this.anotadorservice.getAllGasto().subscribe(resp =>{
   console.log(resp);
   this.gastos =resp;
+  this.gastosfiltrado = resp;
       })
     }
    onSubmit() {
      console.log(this.model);
      this.anotadorservice.addGasto(this.model).subscribe(resp =>{
       console.log(resp);
-    
+
+      this.anotadorservice.getAllGasto().subscribe(resp =>{
+        console.log(resp);
+        this.gastos=resp;
+        this.gastosfiltrado=resp;
+      })
    })}
-   buscar($event: Event) {
- //   this.titulobuscar = $event.detail.value;
-    console.log($event)
+   buscar($param: string) {
+ this.gastosfiltrado = this.gastos.filter(gastos => gastos.titulo.includes($param))
+
   }
 }
-    
